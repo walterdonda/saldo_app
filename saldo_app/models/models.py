e3440c1675a1a1d16cf866db5feb092824f4040b
Walter Donda <walteroscardonda@gmail.com>
2023-07-23 20:55:56 -0300

    Declaro modelos y vistas proncipales

--- saldo_app/models/models.py
+++ saldo_app/models/models.py
@@ -13,6 +13,9 @@ class Movimiento(models.Model):
     date = fields.Datetime(string= 'Fecha de movimiento')
     move_amount = fields.Float('Monto del movimiento')
     receipt_image = fields.Binary('Imagen adjunta del recibo')
+    user_id = fields.Many2one('res.users', string='Usuario')
+    category_id = fields.Many2one('saldo_app.category', string='Categoría')
+    tag_ids = fields.Many2many('saldo_app.tag', string='Tags')
 
 class Category(models.Model):
     _name = 'saldo_app.category'
@@ -22,7 +25,13 @@ class Category(models.Model):
 
 class Tag(models.Model):
     _name = 'saldo_app.tag'
-    _description = 'Tag'
+    _description = 'Etiquetas de movimientos'
 
     name = fields.Char('Nombre')
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+    movimiento_ids = fields.One2many('saldo_app.movimiento', 'user_id', string='Movimientos')
+    
+    
         
