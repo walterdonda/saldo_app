8c7cb770147f7f4aed72e37b95eb3f4d21fce833
Walter Donda <walteroscardonda@gmail.com>
2023-07-28 21:20:34 -0300

    Agrego soporte multimoneda

--- saldo_app/models/models.py
+++ saldo_app/models/models.py
@@ -4,18 +4,18 @@ class Movimiento(models.Model):
     _name = 'saldo_app.movimiento'
     _description = 'Movimiento'
 
-    name = fields.Char('Nombre')
+    name = fields.Char(string = 'Descripción')
     move_type = fields.Selection([
         ('ingreso', 'Ingreso'),
-        ('egreso', 'Egreso')
-
-    ], string='Tipo de movimiento')
-    date = fields.Datetime(string= 'Fecha de movimiento')
-    move_amount = fields.Float('Monto del movimiento')
+        ('egreso', 'Egreso')], string='Tipo de movimiento')
+    date = fields.Date('Fecha del movimiento')
+    move_amount = fields.Monetary(string ='Monto')
+    currency_id = fields.Many2one('res.currency', string='Moneda', default=lambda self: self.env.company.currency_id)
     receipt_image = fields.Binary('Imagen adjunta del recibo')
     user_id = fields.Many2one('res.users', string='Usuario')
     category_id = fields.Many2one('saldo_app.category', string='Categoría')
-    tag_ids = fields.Many2many('saldo_app.tag', string='Tags')
+    tag_ids = fields.Many2many('saldo_app.tag', string='Etiquetas')
+    notas = fields.Html('Notas')
 
 class Category(models.Model):
     _name = 'saldo_app.category'
