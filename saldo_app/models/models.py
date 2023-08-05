a57481e8ac629f22d13cb5f34109b9eec8dbeb62
Walter Donda <walteroscardonda@gmail.com>
2023-08-05 19:46:31 -0300

    Agrego manú "Mi cuenta" para movimientos, acciones de servidor mejorada

--- saldo_app/models/models.py
+++ saldo_app/models/models.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from odoo.exceptions import ValidationError
 
 
 class Movimiento(models.Model):
@@ -14,6 +15,7 @@ class Movimiento(models.Model):
     )
     date = fields.Date("Fecha del movimiento", tracking=True)
     move_amount = fields.Monetary(string="Monto", tracking=True)
+    user_id = fields.Many2one('res.users', string='Usuario', default= lambda self: self.env.user.id)
     company_id = fields.Many2one(
         "res.company", string="Empresa", default=lambda self: self.env.company.id
     )
@@ -24,10 +26,17 @@ class Movimiento(models.Model):
         tracking=True,
     )
     receipt_image = fields.Binary("Imagen adjunta del recibo", tracking=True)
-    user_id = fields.Many2one("res.users", string="Usuario")
+    user_id = fields.Many2one("res.users", string="Usuario",default=lambda self: self.env.user.id)
     category_id = fields.Many2one("saldo_app.category", string="Categoría")
     tag_ids = fields.Many2many("saldo_app.tag", string="Etiquetas")
     notas = fields.Html("Notas", tracking=True)
+    @api.constrains('move_amount')
+    def _constrains_move_amount(self):
+        if(self.move_amount < 0):
+            raise ValidationError("El monto del movimiento no puede ser menor que 0")
+        elif(self.move_amount > 100000):
+            raise ValidationError("El monto del movimiento no puede ser mayor que 100000")
+        
 
 
 class Category(models.Model):
@@ -37,7 +46,14 @@ class Category(models.Model):
     name = fields.Char("Nombre")
 
     def ver_movimientos(self):
-        pass
+        return{
+            "name":f"Movimientos de la categoría  {self.name}",
+            "type": "ir.actions.act_window",
+            "res_model": "saldo_app.movimiento",
+            "views": [[False,"tree"]],
+            "target": "new",
+            "domain": [["category_id",'=',self.id]]
+        }
 
 
 class Tag(models.Model):
@@ -52,3 +68,14 @@ class ResUsers(models.Model):
     movimiento_ids = fields.One2many(
         "saldo_app.movimiento", "user_id", string="Movimientos"
     )
+
+    def mi_cuenta(self):
+        return {
+            "name": f"Mi cuenta",
+            "type": "ir.actions.act_window",
+            "res_model": "res.users",
+            "res_id": self.env.user.id,
+            "views": [[False, "form"]],
+            "target": "self"
+        }
+        
